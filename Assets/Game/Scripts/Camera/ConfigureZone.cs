80e24d630717614d592a8ad170ff91fde35870eb
stban07 <82669737+stban07@users.noreply.github.com>
2025-03-30 18:09:31 -0300

    Feat (camera) Dynamic confiner boundary

---
+++ Assets/Game/Scripts/Camera/ConfigureZone.cs
@@ -0,0 +1,48 @@
+using System;
+using Unity.Cinemachine;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ConfigureZone : MonoBehaviour {
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	public Tilemap map;
+	public BoxCollider2D cameraBounds;
+	public BoxCollider2D cameraTrigger;
+	public CinemachineConfiner2D confiner;
+	[Range(0, 1)]
+	public int tileOffsetLeft;
+	[Range(0, 1)]
+	public int tileOffsetRight;
+	[Range(0, 1)]
+	public int tileOffsetTop;
+	[Range(0, 1)]
+	public int tileOffsetBottom;
+	[Range(30, 50)]
+	public int skySize = 40;
+	[Range(10, 50)]
+	public int groundSize = 30;
+	void Start() {
+		map.CompressBounds();
+		Vector3Int mapBounds = map.cellBounds.size;
+		float tileSize = map.cellSize.x;
+
+		Vector2 offset = new Vector2(
+			(float)((float)(tileOffsetRight - tileOffsetLeft) / 2.0 + map.cellBounds.center.x),
+			(float)((float)(tileOffsetTop - tileOffsetBottom + skySize - groundSize) / 2.0 + map.cellBounds.center.y))
+			* tileSize;
+		Vector2 size = new Vector2(
+			mapBounds.x + tileOffsetRight + tileOffsetLeft,
+			mapBounds.y + tileOffsetTop + tileOffsetBottom + skySize + groundSize);
+		cameraBounds.offset = offset;
+		cameraBounds.size = size;
+		cameraTrigger.offset = offset;
+		cameraTrigger.size = size - new Vector2(2, 2) * tileSize;
+		confiner.InvalidateBoundingShapeCache();
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
--- Assets/Game/Scripts/Player/PlayerCameraTransition.cs
+++ Assets/Game/Scripts/Player/PlayerCameraTransition.cs
@@ -2,25 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Cinemachine;
-public class PlayerCameraTransition : MonoBehaviour
-{
-    public string triggerTag;
-    public CinemachineCamera primaryCamera;
-    public CinemachineCamera[] allCameras;
-    private void Start(){
-        switchToCamera(primaryCamera);
-    }
-    private void OnTriggerEnter2D(Collider2D other){
-        print("trigger");
-        if(other.CompareTag(triggerTag)){
-            CinemachineCamera target = other.GetComponentInChildren<CinemachineCamera>();
-            switchToCamera(target);
-            print("change area");
-        }
-    }
-    private void switchToCamera(CinemachineCamera target){
-        foreach(var cam in allCameras){
-            cam.enabled=cam==target;
-        }
-    }
+public class PlayerCameraTransition : MonoBehaviour {
+	public string triggerTag;
+	public CinemachineCamera primaryCamera;
+	public CinemachineCamera[] allCameras;
+	private void Start() {
+		switchToCamera(primaryCamera);
+	}
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag(triggerTag)) {
+			CinemachineCamera target = other.GetComponentInChildren<CinemachineCamera>();
+			switchToCamera(target);
+			print("change area");
+		}
+	}
+	private void switchToCamera(CinemachineCamera target) {
+		foreach (var cam in allCameras) {
+			cam.enabled = cam == target;
+			//cam.GetComponent<BoxCollider2D>().isTrigger = cam == target;
+		}
+	}
 }
