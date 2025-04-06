2583f89dcb438c17010b4645a50fb30de6ef92d8
stban07 <82669737+stban07@users.noreply.github.com>
2025-04-05 23:30:21 -0400

    fix (camera) set first camera on Awake

--- Assets/Game/Scripts/Player/PlayerCameraTransition.cs
+++ Assets/Game/Scripts/Player/PlayerCameraTransition.cs
@@ -3,33 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Cinemachine;
 using Unity.VisualScripting;
-public class PlayerCameraTransition : MonoBehaviour {
+public class PlayerCameraTransition : MonoBehaviour
+{
 	public string triggerTag;
 	public CinemachineCamera primaryCamera;
 	public CinemachineCamera[] allCameras;
-	private void Start() {
+	private void Start()
+	{
 		switchToCamera(primaryCamera);
 	}
-	private void OnTriggerEnter2D(Collider2D other) {
-		if (other.CompareTag("Damage")) {
+	private void Awake()
+	{
+		switchToCamera(primaryCamera);
+	}
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag("Damage"))
+		{
 			GetComponentInParent<PlayerDeath>().Respawn();
 			return;
 		}
-		if (other.CompareTag(triggerTag)) {
+		if (other.CompareTag(triggerTag))
+		{
 			CinemachineCamera target = other.GetComponentInChildren<CinemachineCamera>();
 			PlayerDeath player = GetComponentInParent<PlayerDeath>();
 			BoxCollider2D newZone = other.GetComponent<BoxCollider2D>();
 			switchToCamera(target);
-			if (newZone != null & player != null) {
+			if (newZone != null & player != null)
+			{
 				player.SetCurrentZone(newZone);
 			}
 			else { print("no new zone"); }
 		}
 	}
-	private void switchToCamera(CinemachineCamera target) {
-		foreach (var cam in allCameras) {
+	private void switchToCamera(CinemachineCamera target)
+	{
+		foreach (var cam in allCameras)
+		{
 			cam.Priority = cam == target ? 1 : 0;
-			//cam.GetComponent<BoxCollider2D>().isTrigger = cam == target;
 		}
 	}
 }
